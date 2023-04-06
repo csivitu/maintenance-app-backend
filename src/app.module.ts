66245aad2b4ea2e009689521f4508d2b725266b2
Kaushal Rathi <kaushalrathi24@gmail.com>
2023-04-07 04:14:25 +0530

    feat: made redis config async

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,35 +6,26 @@ import { StudentModule } from './student/student.module';
 import { CleaningModule } from './cleaning/cleaning.module';
 import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     AuthModule,
-    // CacheModule.registerAsync({
-    //   useFactory: async () => {
-    //     return {
-    //       store: await redisStore({
-    //         ttl: 60 * 1 * 1000,
-    //         socket: {
-    //           host: 'localhost',
-    //           port: 6379,
-    //         },
-    //       }),
-    //       isGlobal: true,
-    //     };
-    //   },
-    // }),
-    CacheModule.register({
-      store: redisStore,
-      ttl: 60 * 1 * 1000,
-      socket: {
-        host: 'localhost',
-        port: 6379,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: await redisStore({
+            socket: {
+              host: configService.get('REDIS_HOST'),
+              port: configService.get('REDIS_PORT'),
+            },
+          }),
+        };
       },
-      isGlobal: true,
+      inject: [ConfigService],
     }),
     PrismaModule,
     StudentModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { MailModule } from '../mail/mail.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [JwtModule.register({ global: true }), MailModule],
+  imports: [
+    JwtModule.register({ global: true }),
+    MailModule,
+    CacheModule.register({}),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
